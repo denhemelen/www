746c7b6c5278597208c0982c5586c75d2908707e
denhemelen <denys2017vk@gmail.com>
2024-11-01 11:42:26 +0200

    Тут ми вже добавили файл щоб почати 5 лабораторну

---
+++ js/class.js
@@ -0,0 +1,15 @@
+'use strict';
+function School(student, klac){
+    this.student = student;
+    this.klac = klac;
+}
+const studentschool = new School ('Ivan', '10');
+console.log(${this.name});
+
+class Student{
+constructor(name, age){
+    name = new name
+
+}
+}
+console
